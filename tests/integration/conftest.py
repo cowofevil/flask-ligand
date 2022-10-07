908b20a5f07c4197743f446d1539d1605ebc1191
Ryan Gard <ryan.a.gard@msn.com>
2022-10-07 16:00:00 -0700

    [FIX] Fix Integration Test Suite Setup
    Removed the deprecated OIDC_REALM setting that was being set in the integration
    test suite setup.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -190,7 +190,6 @@ def basic_flask_app(
     override_settings = {
         "OIDC_DISCOVERY_URL": f"http://{int_testing_env_vars['KC_HOSTNAME']}:{int_testing_env_vars['KC_PORT']}/"
         f"realms/{int_testing_env_vars['KC_REALM']}/.well-known/openid-configuration",
-        "OIDC_REALM": f"{int_testing_env_vars['KC_REALM']}",
         "SQLALCHEMY_DATABASE_URI": db_uri,
         "DB_AUTO_UPGRADE": True,
         "DB_MIGRATION_DIR": migration_directory,
